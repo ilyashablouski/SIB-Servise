ce24e8f07a56380135061b0cdb9e9d2b71d0abc3
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-02-28 17:30:18 +0300

    reinitialize git story with issues

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+// Plugins
+const {parallel, series, watch, src, dest} = require('gulp');
+const pug = require('gulp-pug');
+const less = require('gulp-less');
+const gcmq = require('gulp-group-css-media-queries');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const smartgrid = require('smart-grid');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+
+// Globs
+const config = {
+  root: './dev/',
+  pug: {
+    src: 'pug/+(index).pug',
+    watch: 'pug/**/*.pug',
+    dest: './prod',
+  },
+  css: {
+    src: 'less/+(styles).less',
+    watch: 'less/**/*.less',
+    dest: './prod/css',
+  },
+  js: {
+    src: 'js/+(common).js',
+    watch: 'js/**/*.js',
+    prod: './prod/js',
+  },
+};
+
+/**
+ * Compile pug to html
+ *
+ * @return {string} Return file's paths
+ */
+function html() {
+  return src(config.root + config.pug.src)
+      .pipe(pug({
+        pretty: true,
+      }))
+      .pipe(dest(config.pug.dest));
+}
+
+/**
+ * Compile less to css
+ *
+ * @return {string} Return file's paths
+ */
+function css() {
+  return src(config.root + config.css.src)
+      .pipe(less())
+      .pipe(gcmq())
+      .pipe(dest(config.css.dest))
+      .pipe(browserSync.stream())
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+      }))
+      .pipe(cleanCSS({
+        level: 2,
+      }))
+      .pipe(rename({
+        extname: '.min.css',
+      }))
+      .pipe(dest(config.css.dest));
+}
+
+/**
+ * Initialize smart-grid library
+ *
+ * @param {*} done End async function
+ */
+function grid(done) {
+  smartgrid('./dev/less', {
+    offset: '1.5rem',
+    container: {
+      maxWidth: '1140px',
+    },
+  });
+  done();
+}
+
+/**
+ * Initialize live reload
+ *
+ * @param {*} done End async function
+ */
+function livereload(done) {
+  browserSync.init({
+    server: {
+      baseDir: './prod/',
+    },
+  });
+
+  done();
+}
+
+/**
+ * Task's assignment
+ */
+exports.html = html;
+exports.css = css;
+exports.grid = grid;
+// Build final bundle from pug, less, js
+exports.build = parallel(html, css);
+// Watch changes from pug, less, js
+exports.watch = series(parallel(html, css), livereload,
+    function() {
+      watch(config.root + config.pug.watch, series(html, function(done) {
+        browserSync.reload();
+
+        done();
+      }));
+
+      watch(config.root + config.css.watch, css);
+    });
