9d4b177d40bc8fc969a992d82fa8f628e775d61d
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-03-05 16:56:16 +0300

    add popup full logic and slider

--- prod/js/common.js
+++ prod/js/common.js
@@ -1,108 +1,60 @@
-/**
- * Define variables
- */
-const infoButton = document.querySelector('.header-nav-info');
-// const catalogButton = document.querySelector('.header-nav-catalog');
-const photoIconButton = document.querySelector('.min-photo');
-const dropdownBlock = document.querySelector('.dropdown-block');
-
-
-// Define functions
-/**
- * Open/close element through display
- */
-function openInfoElement() {
-  dropdownBlock.classList.toggle('is-show');
-}
-
-// DOM loaded
-window.addEventListener('DOMContentLoaded', () => {
-  // Close pop-up if clicked other areas
-  $(document).mouseup(function(e) { // событие клика по веб-документу
-    const div = $('.pop-up-js'); // тут указываем class элемента
-    if (!div.is(e.target) && // если клик был не по нашему блоку
-      div.has(e.target).length === 0) { // и не по его дочерним элементам
-      dropdownBlock.classList.remove('is-show'); // скрываем его
-      console.log('Yes');
-    }
-  });
-
-  infoButton.addEventListener('click', openInfoElement);
-
-  photoIconButton.addEventListener('click', openInfoElement);
-});
-
-
-// function showModalFirst() {
-//   event.preventDefault();
-//   document.body.classList.add('overflow-hidden');
-//   modalNodeOther.classList.remove('modal-container_close');
-// }
-
 // /**
-//   Show first modal node's definition
+//  * Define variables
 //  */
-// function showModalFirst() {
-//   event.preventDefault();
-//   document.body.classList.add('overflow-hidden');
-//   modalNodeOther.classList.remove('modal-container_close');
-// }
+// const infoButton = document.querySelector('.header-nav-info');
+// // const catalogButton = document.querySelector('.header-nav-catalog');
+// const dropdownBlock = document.querySelector('.dropdown-block');
+// const tableElement = document.querySelector('.scheme-table');
+// const popupElements = document.querySelectorAll('.pop-up-js-window');
 
+
+// // Define functions
 // /**
-//   Show second modal node's definition
+//  * Open/close element
 //  */
-// function showModalSecond() {
-//   event.preventDefault();
-//   document.body.classList.add('overflow-hidden');
-//   modalNodeUpdate.classList.remove('modal-container_close');
+// function openInfoElement() {
+//   dropdownBlock.classList.toggle('is-show');
 // }
 
 // /**
-//  * Close second modal node's definition
+//  * Open/close pop-up
 //  *
-//  * @param {object} currentNode -текущий элемент
+//  * @param {object} target
 //  */
-// function closeModal(currentNode) {
-//   event.preventDefault();
-//   document.body.classList.remove('overflow-hidden');
-//   currentNode.closest('.we-modal').classList.add('modal-container_close');
+// function showPopup(target) {
+//   // If we in photo-icon element
+//   if (target.classList.contains('photo-icon')) {
+//     const popupWindow = target.parentElement.nextElementSibling;
+//     popupWindow.classList.toggle('is-show');
+//   // If we in photo-icon__inner element
+//   } else if (target.classList.contains('photo-icon__inner')) {
+//     const popupWindow = target.parentElement.parentElement.nextElementSibling;
+//     popupWindow.classList.toggle('is-show');
+//   }
 // }
 
-// DOM loaded
+// // DOM loaded
 // window.addEventListener('DOMContentLoaded', () => {
+//   // Close pop-up if clicked other areas
+//   $(document).mouseup(function(e) { // Document click event
+//     const $popupElement = $('.pop-up-js'); // Element's class
+//     if (!$popupElement.is(e.target) && // Not our block
+//       $popupElement.has(e.target).length === 0) { // Not sons
+//       for (const elem of popupElements) {
+//         elem.classList.remove('is-show');
+//       }
+//     }
+//   });
+
+//   // Call open info element event
+//   infoButton.addEventListener('click', openInfoElement);
+
 //   /**
-//    * Add styles to nav with scroll
+//    * Dilocation and call events
 //    */
-//   window.addEventListener('scroll', function() {
-//     if (window.pageYOffset >= 44) {
-//       stickNode.classList.add('localnav-sticking');
-//     } else {
-//       stickNode.classList.remove('localnav-sticking');
-//     }
-//   });
-
-//   /**
-//   Dilocation and call events
-//  */
-//   mainNode.addEventListener('click', (event) => {
+//   tableElement.addEventListener('click', (event) => {
 //     const target = event.target;
-//     if (target.classList.contains('button-modal-1')) {
-//       showModalFirst();
-//     } else if (target.classList.contains('button-modal-2')) {
-//       showModalSecond();
-//     } else {
-//       return;
-//     }
-//   });
 
-//   // Overlay close areas
-//   mainModalNode.addEventListener('click', (event) => {
-//     const target = event.target;
-//     if (target.classList.contains('close-button') ||
-//         target.classList.contains('overlay')) {
-//       closeModal(target);
-//     } else {
-//       return;
-//     }
+//     showPopup(target);
 //   });
 // });
